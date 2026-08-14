b43c9e256bf78e9713718be436d34b8ef3284f49
aakashdriyan6396-crypto <aakashdriyan6396@gmail.com>
2026-08-14 21:04:53 +0530

    configure production api

--- FrontEnd/src/api/axios.js
+++ FrontEnd/src/api/axios.js
@@ -1,7 +1,16 @@
+// // import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: "http://localhost:7777/api",
+// });
+
+// export default api;
+
+
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:7777/api",
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
 });
 
 export default api;
